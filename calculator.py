99b19b467f43c92f9045af9d73e750e6793889fe
Moeed <ppro78741@gmail.com>
2021-03-07 23:12:32 +0330

    sleepy

--- calculator.py
+++ calculator.py
@@ -1,1 +1,44 @@
-calculator.py
+import cv2 
+
+class draw(object):
+	"""
+	draw the calculator
+	"""
+	def __init__(self,num_color,box_color):
+		self.num_color = num_color 
+		self.box_color = box_color 
+		self.signs = [['0','1','2','+'],
+				['3','4','5','-'],
+				['6','7','8','x'],
+				['9','.','/','=']]
+
+	def Draw(self,frame,colors,pressed=False,x=0,y=0):
+		x,y = 100,100
+		for row in self.signs:
+			for sign in row:
+				cv2.rectangle(frame,(x,y),(x+80,y+80),colors[1],2)
+				cv2.putText(frame,sign,(x+40,y+40),cv2.FONT_HERSHEY_TRIPLEX,0.7,colors[0],2)
+				if pressed:
+					pass
+				x += 80 
+			y += 80
+			x = 100
+		return frame
+
+	def getColor(self):
+		return self.num_color,self.box_color
+
+class calculate:
+	"""
+	calculate phrase
+	"""
+	def __init__(self):
+		pass 
+
+	def Calc(self,phrase=""):
+		pass 
+
+	def click(self,area):
+		if area >= 3000:
+			print(area)
+		return False
--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 import cv2 
 import numpy as np 
+from calculator import draw,calculate
 
 cam = cv2.VideoCapture(1,cv2.CAP_DSHOW)  
 l_green = (36,80,0) 
 h_green = (86,255,255)
 pre_area = 0 
 area = 0
-def findObject(image):
-	global l_green,h_green,pre_area,area
+machine = calculate()
+pen = draw((255,0,0),(0,0,255))
+def findObject(org,image):
+	global frame,l_green,h_green,pre_area,area
 	hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 	mask = cv2.inRange(hsv,l_green,h_green)
 	cnts,_ = cv2.findContours(mask,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 	if len(cnts) > 0:
 		cnt = max(cnts,key=cv2.contourArea) 
 		area = cv2.contourArea(cnt)
-		if area >= 1000:
+		if area >= 1200:
 			M = cv2.moments(cnt)
 			X = int(M['m10'] / M['m00'])
 			Y = int(M['m01'] / M['m00'])
 			if pre_area != 0:
 				pressed = area-pre_area 
-				if pressed >= 500:
-					pass
+				org = pen.Draw(org,pen.getColor(),pressed=machine.click(pressed),x=X,y=Y)
+				return org
 			pre_area = area
+	return []
 
-while True:
-	frame = cam.read()[1]
-	frame = cv2.flip(frame,1)
-	blurred = cv2.GaussianBlur(frame,(9,9),0)
-	findObject(blurred)
-	cv2.imshow('UNNAMED',frame)
-	if cv2.waitKey(1) == ord("q"):
-		quit()
+def main():
+	while True:
+		frame = cam.read()[1]
+		frame = cv2.flip(frame,1)
+		blurred = cv2.GaussianBlur(frame,(9,9),0)
+		obj = findObject(frame,blurred)
+		if len(obj) > 0:
+			frame = obj
+		cv2.imshow('UNNAMED',frame)
+		if cv2.waitKey(1) == ord("q"):
+			quit()
+
+if __name__ == '__main__':
+	main()
