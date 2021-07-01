a553bf5ac9db392780accafcddbba12fbe63348f
Moeed <72065720+Moeed1mdnzh@users.noreply.github.com>
2021-07-01 09:47:03 +0430

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 import cv2 
 
-class draw(object):
+class draw:
 	"""
 	draw the calculator
 	"""
@@ -14,11 +14,11 @@ class draw(object):
 				['9','.','/','=','C']]
 
 	def Draw(self,frame,colors,phrase,pressed=False,X=0,Y=0):
-		x,y = 100,100
+		x,y = 20,30
 		for row in self.signs:
 			for sign in row:
-				cv2.rectangle(frame,(x,y),(x+80,y+80),colors[1],10)
-				cv2.putText(frame,sign,(x+35,y+45),cv2.FONT_HERSHEY_TRIPLEX,0.9,colors[0],3)
+				cv2.rectangle(frame,(x,y),(x+40,y+40),colors[1],7)
+				cv2.putText(frame,sign,(x+17,y+22),cv2.FONT_HERSHEY_TRIPLEX,0.7,colors[0],2)
 				if pressed:
 					SIGN = self.machine.operate(phrase,(X,Y))
 					if SIGN == 'C': 
@@ -29,9 +29,10 @@ class draw(object):
 						phrase = self.machine.Calc(phrase)
 					else: phrase += SIGN
 					pressed = False
-				x += 80 
-			y += 80
-			x = 100
+				x += 40 
+			y += 40
+			x = 20
+			cv2.circle(frame,(X,Y),4,(0,0,240),-1)
 		return frame,phrase
 
 	def getColor(self):
@@ -42,10 +43,10 @@ class calculate:
 	calculate phrase
 	"""
 	def __init__(self):
-		self.boxes = {'0':(100,100),'1':(180,100),'2':(260,100),'+':(340,100),
-				'3':(100,180),'4':(180,180),'5':(260,180),'-':(340,180),
-				'6':(100,260),'7':(180,260),'8':(260,260),'x':(340,260),'D':(420,260),
-				'9':(100,340),'.':(180,340),'/':(260,340),'=':(340,340),'C':(420,340)}
+		self.boxes = {'0':(20,30),'1':(60,30),'2':(100,30),'+':(140,30),
+				'3':(20,80),'4':(60,80),'5':(100,80),'-':(140,80),
+				'6':(20,110),'7':(60,110),'8':(100,110),'x':(140,110),'D':(180,110),
+				'9':(20,150),'.':(60,150),'/':(100,150),'=':(140,150),'C':(180,150)}
 
 	def Calc(self,phrase=""):
 		try:
@@ -57,11 +58,11 @@ class calculate:
 	def operate(self,phrase,dot):
 		x,y = dot 
 		for k,v in self.boxes.items():
-			if (x >= v[0] and x <= v[0]+79) and (y >= v[1] and y <= v[1]+79):
+			if (x >= v[0] and x <= v[0]+39) and (y >= v[1] and y <= v[1]+39):
 				return k
 		return ''
 
-	def click(self,area):
-		if area >= 1800:
+	def click(self,dist):
+		if dist >= 15:
 			return True
 		return False
--- main.py
+++ main.py
@@ -1,47 +1,50 @@
 import cv2 
 import numpy as np 
+import mediapipe as mp
 from calculator import draw,calculate
 
 cam = cv2.VideoCapture(0)  
-l_green = (36,80,0) 
-h_green = (86,255,255)
-pre_area = 0 
-area = 0
 machine = calculate()
 pen = draw((130,130,255),(255,180,180))
 phrase = ''
-def findObject(org,image):
-	global frame,l_green,h_green,pre_area,area,phrase
-	hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-	mask = cv2.inRange(hsv,l_green,h_green)
-	cnts,_ = cv2.findContours(mask,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-	if len(cnts) > 0:
-		cnt = max(cnts,key=cv2.contourArea) 
-		area = cv2.contourArea(cnt)
-		if area >= 1200:
-			M = cv2.moments(cnt)
-			X = int(M['m10'] / M['m00'])
-			Y = int(M['m01'] / M['m00'])
-			if pre_area != 0:
-				pressed = area-pre_area 
-				org,phrase = pen.Draw(org,pen.getColor(),phrase,pressed=machine.click(pressed),X=X,Y=Y)
-				cv2.putText(org,phrase,(100,70),cv2.FONT_HERSHEY_TRIPLEX,1,(130,255,130),2)
-				pre_area = area
+hand_base = mp.solutions.hands
+hand = hand_base.Hands()
+pre_x,pre_y = None,None
+def findFinger(image,H,W):
+	global phrase,pre_x,pre_y
+	clone = image.copy()
+	rgb = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+	res = hand.process(rgb)
+	if res.multi_hand_landmarks:
+		for hand_landmarks in res.multi_hand_landmarks:
+			finger = hand_landmarks.landmark[hand_base.HandLandmark.INDEX_FINGER_TIP]
+			X,Y = int(finger.x*W),int(finger.y*H)
+			if pre_y is not None: 
+				org,phrase = pen.Draw(clone,pen.getColor(),phrase,pressed=machine.click(Y-pre_y),
+					X=int(np.mean(np.array([X,pre_x]))),
+					Y=int(np.mean(np.array([Y,pre_y])))
+					)
+				cv2.putText(org,phrase,(20,15),cv2.FONT_HERSHEY_TRIPLEX,0.6,(130,255,130),2)
+				pre_x,pre_y = X,Y
 				return org
-			pre_area = area
-	return []
+			pre_x,pre_y = X,Y
+		return clone
+	return clone
+
+
 
 def main():
 	while True:
 		frame = cam.read()[1]
+		frame = cv2.resize(frame,(448,336))
 		frame = cv2.flip(frame,1)
-		blurred = cv2.GaussianBlur(frame,(9,9),0)
-		obj = findObject(frame,blurred)
-		if len(obj) > 0:
-			frame = obj
+		frame = findFinger(frame,336,448)
 		cv2.imshow('UNNAMED',frame)
-		if cv2.waitKey(1) == ord("q"):
+		if cv2.waitKey(30) == ord("q"):
 			quit()
 
 if __name__ == '__main__':
 	main()
+
+"""org,phrase = pen.Draw(org,pen.getColor(),phrase,pressed=machine.click(pressed),X=X,Y=Y)
+cv2.putText(org,phrase,(100,70),cv2.FONT_HERSHEY_TRIPLEX,1,(130,255,130),2)"""
